f610dad25bb7833391a69c4780fac709c8e55ea3
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2026-08-21 15:13:31 +0600

    build: initialize Next.js production build artifacts and update .gitignore

--- Trash/assets/js/home_script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const scrollDown = document.querySelector('.scroll-down');
-  
-    scrollDown.addEventListener('click', function(event) {
-      event.preventDefault();
-      document.querySelector('#content').scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-  
